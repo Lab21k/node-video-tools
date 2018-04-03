dc91e73606f199932ed2011c45979644bb6d8c69
André Abadesso <andre@lab21k.com.br>
2018-04-03 01:14:05 -0300

    feat: black screen with font on first 5s

--- font.js
+++ font.js
@@ -5,13 +5,24 @@ boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2" -codec:a copy*/
 let ffmpeg = require('fluent-ffmpeg');
 let Promise = require('bluebird')
 
-ffmpeg('./videos/black.mov')
-    .addOption('-vf', `drawtext="fontfile=./fonts/SIXTY.TTF: text='Título do imóvel, que pode ser bem grande ou bem pequeno, tratar isso...': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2"`)
-    .addOption('-strict', 'experimental')
-    .on('end', () => {
-        console.log('DOne')
-    })
-    .on('error', (err) => {
-        console.log(err)
-    })
-    .saveToFile('./output_audio.mp4')
+ffmpeg('./black.mp4')
+//.addOption('-vf', `drawtext="fontfile=./fonts/SIXTY.TTF: text='Título do imóvel, que pode ser bem grande ou bem pequeno, tratar isso...': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2"`)
+  .addOption('-strict', 'experimental')
+  .complexFilter({
+    filter: 'drawtext',
+    options: {
+      'fontfile': 'SIXTY.TTF',
+      'text': 'HUAEUHAUEHAUEHUAHEAE ALL YOUR BASE ARE BELONG TO US',
+      fontsize: 24,
+      fontcolor: 'white',
+      x: '(w-text_w)/2',
+      y: '(h-text_h)/2'
+    }
+  })
+  .on('end', () => {
+      console.log('Done')
+  })
+  .on('error', (err) => {
+      console.log(err)
+  })
+  .saveToFile('./output_audio.mp4')
--- index.js
+++ index.js
@@ -1,43 +1,66 @@
 let ffmpeg = require('fluent-ffmpeg');
 let Promise = require('bluebird')
 
-module.exports = (filePaths, outPath, audioPath) => {
+module.exports = (filePaths, outPath, audioPath, text, fontPath) => {
     let rand = Math.random().toString(36).substring(7)
-    let tempName = `/tmp/temp_video_${rand}.mp4`
+    let temp = `/tmp/temp_video_${rand}`
+    let tempName = `${temp}.mp4`
 
     return new Promise((resolve, reject) => {
-        let firstVideo = filePaths.shift()
-        let proc = filePaths.reduce((acc, file) => {
-            return acc.input(file)
-        }, ffmpeg(firstVideo)
-            .renice(5)
+        ffmpeg('./black.mp4')
             .addOption('-strict', 'experimental')
-        )
+            .complexFilter({
+                filter: 'drawtext',
+                options: {
+                    fontfile: fontPath,
+                    text: text,
+                    fontsize: 24,
+                    fontcolor: 'white',
+                    x: '(w-text_w)/2',
+                    y: '(h-text_h)/2'
+                }
+            })
+            .on('end', () => {
+              //let firstVideo = filePaths.shift()
+                let proc = filePaths.reduce((acc, file) => {
+                        return acc.input(file)
+                    }, ffmpeg(`${temp}_black.mp4`)
+                    .renice(5)
+                    .addOption('-strict', 'experimental')
+                )
 
-        proc.on('end', () => {
-            ffmpeg()
-                .renice(5)
-                .addOption('-strict', 'experimental')
-                .addOption('-shortest')
-                .addInput(audioPath)
-                .addInput(tempName)
-                .on('end', () => {
-                    resolve()
-                    console.log('Audio filtered.')
+                proc.on('end', () => {
+                    ffmpeg()
+                        .renice(5)
+                        .addOption('-strict', 'experimental')
+                        .addOption('-shortest')
+                        .addInput(audioPath)
+                        .addInput(tempName)
+                        .applyAutoPadding()
+                        .on('end', () => {
+                            resolve()
+                            console.log('Audio filtered.')
+                        })
+                        .on('error', (err) => {
+                            reject(err)
+                            console.log('Audio error', err)
+                        })
+                        .saveToFile(outPath)
                 })
-                .on('error', (err) => {
+
+                proc.on('error', (err, err2, err3) => {
+                    console.log('Error', err.message)
+                    console.log('err2', err2)
+                    console.log('err3', err3)
                     reject(err)
-                    console.log('Audio error', err)
                 })
-                .saveToFile(outPath)
-        })
 
-        proc.on('error', (err) => {
-            console.log('Error', err.message)
-            reject(err)
-        })
-
-        proc
-            .mergeToFile(tempName)
+                proc
+                    .mergeToFile(tempName)
+            })
+            .on('error', (err) => {
+                console.log(err)
+            })
+            .saveToFile(`${temp}_black.mp4`)
     })
 }
